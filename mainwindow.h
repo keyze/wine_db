c0c54ab7ccc3cfe724a9e6f13d03c420266eca77
akeyes <akeyes@barrukka.local>
2017-06-23 14:27:42 +1000

    beginnings of wind db

---
+++ mainwindow.h
@@ -0,0 +1,89 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QActionGroup>
+#include <QLabel>
+#include <QContextMenuEvent>
+#include <QStackedWidget>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void changePage(int);
+    void newFile();
+    void open();
+    void save();
+    void print();
+    void undo();
+    void redo();
+    void cut();
+    void copy();
+    void paste();
+    void bold();
+    void italic();
+    void leftAlign();
+    void rightAlign();
+    void justify();
+    void center();
+    void setLineSpacing();
+    void setParagraphSpacing();
+    void about();
+    void aboutQt();
+
+protected:
+#ifndef QT_NO_CONTEXTMENU
+    void contextMenuEvent(QContextMenuEvent *event) override;
+#endif // QT_NO_CONTEXTMENU
+
+signals:
+    void valueChanged(int newValue);
+
+private:
+    Ui::MainWindow *ui;
+
+    void createActions();
+    void createMenus();
+
+    int m_value;
+
+    QStackedWidget *pagesWidget;
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *formatMenu;
+    QMenu *helpMenu;
+    QActionGroup *alignmentGroup;
+    QAction *newAct;
+    QAction *openAct;
+    QAction *saveAct;
+    QAction *printAct;
+    QAction *exitAct;
+    QAction *undoAct;
+    QAction *redoAct;
+    QAction *cutAct;
+    QAction *copyAct;
+    QAction *pasteAct;
+    QAction *boldAct;
+    QAction *italicAct;
+    QAction *leftAlignAct;
+    QAction *rightAlignAct;
+    QAction *justifyAct;
+    QAction *centerAct;
+    QAction *setLineSpacingAct;
+    QAction *setParagraphSpacingAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+    QLabel *infoLabel;
+};
+
+#endif // MAINWINDOW_H
---
+++ pages.h
@@ -0,0 +1,25 @@
+#ifndef PAGES_H
+#define PAGES_H
+
+#include <QObject>
+#include <QWidget>
+
+class ConfigurationPage : public QWidget
+{
+public:
+    ConfigurationPage(QWidget *parent = 0);
+};
+
+class QueryPage : public QWidget
+{
+public:
+    QueryPage(QWidget *parent = 0);
+};
+
+class UpdatePage : public QWidget
+{
+public:
+    UpdatePage(QWidget *parent = 0);
+};
+
+#endif
